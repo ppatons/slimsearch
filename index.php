2471479d4ae4bcc8881574826d7e409c74f40568
Patrick Patons <ppatons@gmail.com>
2013-09-22 11:51:58 +0000

    The handler for multi-resource GET requests

--- index.php
+++ index.php
@@ -2,7 +2,31 @@
 
 require 'vendor/autoload.php';
 
-// include redbean and use MariaDB / mysql
-require_once($SLIM_ROOT/vendor/RedBean/'rb.php');
+// load required files RedBean
+require_once('vendor/RedBean/rb.php');
+
+// register Slim auto-loader
+\Slim\Slim::registerAutoloader();
+
+// set up database connection
 R::setup('mysql:host=localhost;dbname=slimsearch','root','');
 // R::setup('mysql:host=localhost; dbname=mydatabase','user','password');
+R::freeze(true);
+
+// initialize app
+$app = new \Slim\Slim();
+
+// handle GET requests for /people
+$app->get('/people', function () use ($app) {
+	// query database for all people
+	$people = R::find('people');
+
+	// send response header for JSON content type
+	$app->response()->header('Content-Type', 'application/json');
+
+	// return JSON-encoded response body with query results
+	echo json_encode(R::exportAll($people));
+});
+
+// run
+$app->run();
