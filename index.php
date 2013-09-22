72173c721dc7c502e1eabf5b8ffb66ed1cb98d04
Patrick Patons <ppatons@gmail.com>
2013-09-22 10:13:40 +0000

    lets cook some RedBean

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
+
 require 'vendor/autoload.php';
+
+// include redbean and use MariaDB / mysql
+require_once($SLIM_ROOT/vendor/RedBean/'rb.php');
+R::setup('mysql:host=localhost;dbname=slimsearch','root','');
+// R::setup('mysql:host=localhost; dbname=mydatabase','user','password');
